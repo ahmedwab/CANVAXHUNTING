e560c7038e1b87a26efba11c557471276e11edb7
ahmedwab <52334883+ahmedwab@users.noreply.github.com>
2021-04-29 22:06:07 -0400

    Add files via upload

---
+++ database/posts.sql
@@ -0,0 +1,31 @@
+
+/*
+Table to store emails along with postal codes
+
+Column ID 
+DATATYPE: INTEGER NOT NULL AUTO_INCREMENT
+DESCRIPTION: unique ID of post
+
+
+column LINK
+DATATYPE: TEXT NOT NULL
+DESCRIPTION: Embedded twitter link
+
+column PROVINCE
+DATATYPE: varchar(255) NOT NULL
+DESCRIPTION: Province or territory name
+
+column POSTALCODE
+DATATYPE: varchar(3) NOT NULL
+DESCRIPTION: first 3 characters of postal code that identifies post's address
+
+
+*/
+
+CREATE TABLE POSTS (
+    ID INTEGER NOT NULL AUTO_INCREMENT,
+    LINK TEXT NOT NULL,
+    PROVINCE varchar(255) NOT NULL,
+    POSTALCODE varchar(3) NOT NULL,
+    PRIMARY KEY (ID)
+);
