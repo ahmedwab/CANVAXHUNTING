2d4ed9da0d443a2c42e0bd3dcc42f91ea78f9fe6
ahmedwab <52334883+ahmedwab@users.noreply.github.com>
2021-05-01 21:41:30 -0400

    Add files via upload

---
+++ addposts.php
@@ -0,0 +1,147 @@
+<html>
+   <body>
+   <img src="images/Maple_Leaf.svg" alt="Canada" id="mapleLeaf">
+         <head>
+            <title> Canada Vaccine Search</title>
+            <link rel="stylesheet" href="stylesheets/login-style.css">
+          </head>
+      
+      <div class="container" id="sign-in">
+      <div>
+         <br>
+         <h2 align="center" id="login-txt">Add Post</h2>
+         <form name="login-form" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>" method="post">
+            <div >
+                Embedded Twitter Link: <input type="text" class="form-control" name="tname" placeholder='<blockquote class="twitter-tweet"><p lang="und" dir="ltr">[BC] <a href="https://t.co/jmFa5BMWkC">https://t.co/jmFa5BMWkC</a> <a href="https://twitter.com/hashtag/COVID19?src=hash&amp;ref_src=twsrc%5Etfw">#COVID19</a> <a href="https://twitter.com/hashtag/COVID19Vaccine?src=hash&amp;ref_src=twsrc%5Etfw">#COVID19Vaccine</a> <a href="https://twitter.com/hashtag/COVID19BC?src=hash&amp;ref_src=twsrc%5Etfw">#COVID19BC</a> <a href="https://twitter.com/hashtag/vhcBC?src=hash&amp;ref_src=twsrc%5Etfw">#vhcBC</a></p>&mdash; Vaccine Hunters Canada (@VaxHuntersCan) <a href="https://twitter.com/VaxHuntersCan/status/1387934255041372160?ref_src=twsrc%5Etfw">April 30, 2021</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>"'><br>            </div>
+            <div class="form-group">
+               <label for="province">Please choose a province or territory:</label>
+               <select id="province" name="province">
+                  <option value="Alberta">Alberta</option>
+                  <option value="British Columbia">British Columbia</option>
+                  <option value="Manitoba">Manitoba</option>
+                  <option value="New Brunswick">New Brunswick</option>
+                  <option value="Newfoundland and Labrador">Newfoundland and Labrador</option>
+                  <option value="Nova Scotia">Nova Scotia</option>
+                  <option value="Ontario">Ontario</option>
+                  <option value="Prince Edward Island">Prince Edward Island</option>
+                  <option value="Quebec">Quebec</option>
+                  <option value="Saskatchewan">Saskatchewan</option>
+                  <option value="Northwest Territories">Northwest Territories</option>
+                  <option value="Nunavut">Nunavut</option>
+                  <option value="Yukon">Yukon</option>
+               </select>
+               <br>
+            </div>
+            Postal Code: <input type="text" class="form-control" name="pcode"  placeholder="Ex: M3J M3K M3M M3N M6M M9L M9M M9N" required><br>
+            <button type="submit" class="submit" id="submit" name="submit-login">Submit</button>
+            <br>
+         </form>
+         <a  href="login.php"><h3> Go back</h3></a><br>
+         <a  href="addposts.php"><h3> Reset</h3></a><br>
+      </div>
+      <?php
+         //database information
+         $servername = "mysql.canvaxsearch.dreamhosters.com";
+         $username = "ahmedwal";
+         $password = "discussion1407";
+         $databaseName = "canvaxsearch";
+         
+          $conn = new mysqli($servername,$username,$password,$databaseName);
+         
+          session_start();
+          error_reporting(0);
+          if ($_SESSION["user"] == NULL)
+          {
+             header('Location: index.php');
+          }
+         
+          
+         
+          //if a post request is submitted
+         if ($_SERVER["REQUEST_METHOD"] == "POST") {
+           // collect value of input fields
+           $pname = $_POST['tname'];
+           $postalCode = $_POST['pcode'];
+           $postalcodes = explode (" ", $postalCode); 
+           $province = $_POST['province'];
+         
+           //check if fields are properly inputted.
+           if (empty($pname)||strlen($postalCode)<3) {
+             echo "Name is empty";
+           } else {
+            for($i = 0; $i < count($postalcodes); $i++) {
+              
+             //inserts post information to the posts table
+
+
+
+             $stmt = $conn->prepare("INSERT INTO POSTS (LINK, PROVINCE,POSTALCODE) VALUES (?, ?,?)");
+            $stmt->bind_param("sss", $pname, $province,$postalCode);
+
+            // set parameters and execute
+            $pname = $_POST['tname'];
+            $province = $_POST['province'];
+            $postalCode = $postalcodes[$i];
+            if ($stmt->execute()) { 
+              echo "New record created successfully<br>";
+            } else {
+              echo "Error: " . $sql . "<br>" . mysqli_error($conn) . "<br>";
+            }
+        
+         
+         
+         
+             //sending email notifications to everyone with inputted postal code.
+             $sql = "SELECT EMAIL FROM EMAILS WHERE POSTALCODE = '$postalCode'";
+             $stmt = $conn->prepare($sql); 
+              $stmt->bind_param("i", $id);
+              $stmt->execute();
+              $result = $stmt->get_result(); 
+         
+             if ($result->num_rows > 0) {
+               // output data of each row
+               while($row = $result->fetch_assoc()) {
+                 $email=$row["EMAIL"];
+         
+                 $to = "".$email."";
+                 $subject = "We found Vaccines for you";
+             
+                 $message = '
+                 <html>
+                 <head>
+                 <title>HTML email</title>
+                 </head>
+                 <body>'
+                 .$pname.
+                 //href should be replaced with a link to the unsubscribe page
+                 '</body>
+                 </html>
+                 ';
+         
+               }
+             }
+             
+             
+             // Content type
+             $headers = "MIME-Version: 1.0" . "\r\n";
+             $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+             
+             // More headers
+             // Sender should probably be a no-reply@something.com
+             $headers .= 'From: <no-reply@canvaxsearch.com>' . "\r\n";
+             
+             $wasMailed=mail($to,$subject,$message,$headers);
+             if( $wasMailed == true ) {
+                 echo "Message sent successfully...";
+              }else {
+                 echo "No messages sent...";
+              }
+             
+           }
+          }
+         }
+         $stmt->close();
+         $conn->close();
+         ?>
+   </body>
+</html>
---
+++ forgotpassword.php
@@ -0,0 +1,99 @@
+<?php
+session_start();
+error_reporting(0);
+
+//database information
+$servername = "mysql.canvaxsearch.dreamhosters.com";
+$username = "ahmedwal";
+$password = "discussion1407";
+$databaseName = "canvaxsearch";
+
+ $conn = new mysqli($servername,$username,$password,$databaseName);
+
+
+ $accountusername= $_POST['forgot-email'];
+ 
+
+ $sql="SELECT EMAIL FROM ACCOUNTS WHERE EMAIL='$accountusername' ";
+ $stmt = $conn->prepare($sql); 
+    $stmt->bind_param("i", $id);
+    $stmt->execute();
+    $result = $stmt->get_result(); 
+
+
+
+
+ if ($result -> num_rows > 0){
+    
+ 
+  
+  $stmt = $conn->prepare("INSERT INTO PASSWORD (TOKEN,EMAIL) VALUES (?, ?)");
+  $stmt->bind_param("ss", $emailtext, $accountusername);
+  
+  // set parameters and execute
+  
+  $accountusername= $_POST['forgot-email'];
+
+ $emailtext=md5($accountusername). md5(date_default_timezone_get());
+  $stmt->execute();
+
+
+
+   $to = $accountusername;
+         $subject = "Password recovery";
+         
+         $message = "<b>Click the link to recover password.</b>";
+         $message .= "http://canvaxsearch.com/recoverpassword.php?token=" . $emailtext;
+         
+         $header = "From:no-reply@canvaxsearch.com \r\n";
+         $header .= "MIME-Version: 1.0\r\n";
+         $header .= "Content-type: text/html\r\n";
+         
+         $retval = mail ($to,$subject,$message,$header);
+
+
+ }
+ else {
+     echo "something went wrong";
+ }
+ echo '
+ <div class="container" id="sign-in">
+   <div>
+   <br>
+   <h2 align="center" id="login-txt">An Email will be sent to you if valid</h2><br>
+   <a align="center" href="login.php"><h3> Go back</h3></a><br>
+   
+ </div>';
+
+
+
+
+ $stmt->close();
+ $conn->close();
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Forgot Password</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--  Include the CSS Bootstrap library from a CDN (MaxCDN) by inserting the following line
+ -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="stylesheets/login-style.css">
+<script src="script.js"></script>
+
+</head>
+<body>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
